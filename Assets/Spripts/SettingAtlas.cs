414904c1c585380dbe88f22fc3f693e033eaac1b
YLong <553769193@qq.com>
2019-11-28 18:52:03 +0800

    原始项目

---
+++ Assets/Spripts/SettingAtlas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.U2D;
+
+public class SettingAtlas : AssetPostprocessor
+{
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="texture"></param>
+    /// <param name="assetPath"></param>
+    void OnPostprocessTexture(Texture2D texture,string assetPath)
+    {
+        string AtlasName = new DirectoryInfo(Path.GetDirectoryName(assetPath)).Name;
+        TextureImporter textureImporter = assetImporter as TextureImporter;
+        textureImporter.textureType = TextureImporterType.Sprite;
+        textureImporter.spritePackingTag = AtlasName;
+        textureImporter.mipmapEnabled = false;
+    }
+}
